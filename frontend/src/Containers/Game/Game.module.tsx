94000e82bce650a6f7ec6deba842533e43c6e762
Ali Salam <asalam@macbook-pro-de-ali.home>
2022-02-08 13:59:50 +0100

    collision raquette ok

--- frontend/src/Containers/Game/Game.module.tsx
+++ frontend/src/Containers/Game/Game.module.tsx
@@ -46,6 +46,9 @@ function Game(/*props:any*/) {
     var ctx:any;
     var WIDTH:any;
     var HEIGHT:any;
+    var paddlex:any;
+    var paddleh:any;
+    var paddlew:any;
 
     function init() 
     {
@@ -54,6 +57,13 @@ function Game(/*props:any*/) {
         WIDTH = style.getPropertyValue('width');
         HEIGHT = style.getPropertyValue('height');
         return setInterval(draw, 10); // Exécuter draw() toutes les 10 ms
+    }
+
+    function init_paddle() 
+    {
+        paddlex = 350 / 2;
+        paddleh = 10;
+        paddlew = 75;
     }
 
 
@@ -79,11 +89,31 @@ function Game(/*props:any*/) {
     {
         clear();
         circle(x, y, 10);
+        rect(paddlex, 400-paddleh, paddlew, paddleh);
 
         if (x + dx > 800 || x + dx < 0) // Dépassement à droite ou à gauche
+        {
             dx = -dx;
-        if (y + dy > 400 || y + dy < 0) // Dépassement en bas ou en haut
+        }
+
+        if (y + dy < 0)
+        {
             dy = -dy;
+        }
+        else if (y + dy > 400) 
+        {
+            if (x > paddlex && x < paddlex + paddlew)
+            {
+                // Si la balle rentre en collision avec la raquette, la balle rebondit
+                dy = -dy;
+            }
+            else
+            {
+                x = 800/2;
+                y = 400/2;
+                // Sinon la balle revient au centre
+            }
+        }
        
         x += dx;
         y += dy;
@@ -95,6 +125,7 @@ function Game(/*props:any*/) {
         if (canvas != null)
         {
             init();
+            init_paddle();
         }
     }
 
