7a4ff15f0cf0a102d60439ac953703168dc86391
Ali Salam <asalam@macbook-pro-de-ali.home>
2022-02-10 08:39:48 +0100

    stateGame ok

--- frontend/src/Containers/Game/Game.module.tsx
+++ frontend/src/Containers/Game/Game.module.tsx
@@ -13,13 +13,11 @@ const apiAxios = axios.create({
 
 
 
-function Game(/*props:any*/) {
+function Game() {
 
     const [user, setUser] = useState<any>(null);
     const [activeGame, setActiveGame] = useState<boolean>(false);
-    /*const paddleY= useRef<number>(175);*/
-    
-    
+
     const stateGame = useRef<any>({
         width:Number(0),
         height:Number(0),
@@ -27,16 +25,21 @@ function Game(/*props:any*/) {
         ballY:Number(0),
         ballDx:Number(0),
         ballDy:Number(0),
+        ballR:Number(0),
+        ballColor:String(),
         player1Top:Boolean(false),
         player1Bottom:Boolean(false),
         player1PaddleW:Number(0),
         player1PaddleH:Number(0),
         player1PaddleY:Number(0),
+        player1Color:String(),
         player2Top:Boolean(false),
         player2Bottom:Boolean(false),
         player2PaddleW:Number(0),
         player2PaddleH:Number(0),
-        player2PaddleY:Number(0)
+        player2PaddleY:Number(0),
+        player2Color:String(),
+        paddleSpeed:Number(0)
     });
     
     var canvas:any = document.getElementById(styles.canvas);
@@ -80,14 +83,20 @@ function Game(/*props:any*/) {
         stateGame.current.ballY = stateGame.current.height/2;
         stateGame.current.ballDx = 4;
         stateGame.current.ballDy = 2;
+        stateGame.current.ballR = 10;
+        stateGame.current.ballColor = "#ff861d";
 
         stateGame.current.player1PaddleW = 10;
         stateGame.current.player1PaddleH = 75;
         stateGame.current.player1PaddleY = stateGame.current.height/2 - stateGame.current.player1PaddleH/2;
-
+        stateGame.current.player1Color = "#00A308";
+        
         stateGame.current.player2PaddleW = 10;
         stateGame.current.player2PaddleH = 75;
         stateGame.current.player2PaddleY = stateGame.current.height/2 - stateGame.current.player1PaddleH/2;
+        stateGame.current.player2Color = "#2ddff3";
+
+        stateGame.current.paddleSpeed = 5;
         
         return setInterval(draw, 10); // Exécuter draw() toutes les 10 ms
     }
@@ -165,42 +174,42 @@ function Game(/*props:any*/) {
     {
         //mise a jour balle 
         clear();
-        ctx.fillStyle = "#FFFFFF";
-        circle(stateGame.current.ballX, stateGame.current.ballY, 10); 
+        ctx.fillStyle = stateGame.current.ballColor;
+        circle(stateGame.current.ballX, stateGame.current.ballY, stateGame.current.ballR); 
 
         //mise a jour player 1 si appuis sur une touche
         if (stateGame.current.player1Top === true && stateGame.current.player1PaddleY > 0)
-            stateGame.current.player1PaddleY = stateGame.current.player1PaddleY - 5;
+            stateGame.current.player1PaddleY = stateGame.current.player1PaddleY - stateGame.current.paddleSpeed;
         else if (stateGame.current.player1Bottom === true && stateGame.current.player1PaddleY < stateGame.current.height - stateGame.current.player1PaddleH)
-            stateGame.current.player1PaddleY = stateGame.current.player1PaddleY + 5;
-        ctx.fillStyle = "#00A308";
+            stateGame.current.player1PaddleY = stateGame.current.player1PaddleY + stateGame.current.paddleSpeed;
+        ctx.fillStyle = stateGame.current.player1Color;
         rect(0, stateGame.current.player1PaddleY, stateGame.current.player1PaddleW, stateGame.current.player1PaddleH);
 
         //mise a jour player 2 si appuis sur une touche
         if (stateGame.current.player2Top === true && stateGame.current.player2PaddleY > 0)
-            stateGame.current.player2PaddleY = stateGame.current.player2PaddleY - 5;
+            stateGame.current.player2PaddleY = stateGame.current.player2PaddleY - stateGame.current.paddleSpeed;
         else if (stateGame.current.player2Bottom === true && stateGame.current.player2PaddleY < stateGame.current.height - stateGame.current.player2PaddleH)
-            stateGame.current.player2PaddleY = stateGame.current.player2PaddleY + 5;
-        ctx.fillStyle = "#2ddff3";
+            stateGame.current.player2PaddleY = stateGame.current.player2PaddleY + stateGame.current.paddleSpeed;
+        ctx.fillStyle = stateGame.current.player2Color;
         rect(stateGame.current.width - stateGame.current.player2PaddleW, stateGame.current.player2PaddleY, stateGame.current.player2PaddleW, stateGame.current.player2PaddleH);
 
         // Collision haut et bas
-        if (stateGame.current.ballY + stateGame.current.ballDy + 10 > stateGame.current.height || stateGame.current.ballY + stateGame.current.ballDy - 10 < 0) 
+        if (stateGame.current.ballY + stateGame.current.ballDy + stateGame.current.ballR > stateGame.current.height || stateGame.current.ballY + stateGame.current.ballDy - stateGame.current.ballR < 0) 
         {
             stateGame.current.ballDy = -stateGame.current.ballDy;
         }
 
 
-        if (stateGame.current.ballX + stateGame.current.ballDx - 10 <= stateGame.current.player1PaddleW || 
-            stateGame.current.ballX + stateGame.current.ballDx + 10 >= stateGame.current.width - stateGame.current.player2PaddleW) 
+        if (stateGame.current.ballX + stateGame.current.ballDx - stateGame.current.ballR <= stateGame.current.player1PaddleW || 
+            stateGame.current.ballX + stateGame.current.ballDx + stateGame.current.ballR >= stateGame.current.width - stateGame.current.player2PaddleW) 
         {
-            if ((stateGame.current.ballX + stateGame.current.ballDx - 10 <= stateGame.current.player1PaddleW) && (stateGame.current.ballY >= stateGame.current.player1PaddleY - 10 && stateGame.current.ballY <= stateGame.current.player1PaddleY + stateGame.current.player1PaddleH + 10))//10 = securite
+            if ((stateGame.current.ballX + stateGame.current.ballDx - stateGame.current.ballR <= stateGame.current.player1PaddleW) && (stateGame.current.ballY >= stateGame.current.player1PaddleY - stateGame.current.ballR && stateGame.current.ballY <= stateGame.current.player1PaddleY + stateGame.current.player1PaddleH + stateGame.current.ballR))
             {         
                 // Si la balle rentre en collision avec la raquette du joueur 1 , la balle rebondit
                 stateGame.current.ballDx = -stateGame.current.ballDx;
                 stateGame.current.ballDy = 8 * ((stateGame.current.ballY-(stateGame.current.player1PaddleY + stateGame.current.player1PaddleH/2))/stateGame.current.player1PaddleH);
             }
-            else if ((stateGame.current.ballX + stateGame.current.ballDx + 10 >= stateGame.current.width - stateGame.current.player2PaddleW) && (stateGame.current.ballY >= stateGame.current.player2PaddleY - 10 && stateGame.current.ballY <= stateGame.current.player2PaddleY + stateGame.current.player2PaddleH + 10))
+            else if ((stateGame.current.ballX + stateGame.current.ballDx + stateGame.current.ballR >= stateGame.current.width - stateGame.current.player2PaddleW) && (stateGame.current.ballY >= stateGame.current.player2PaddleY - stateGame.current.ballR && stateGame.current.ballY <= stateGame.current.player2PaddleY + stateGame.current.player2PaddleH + stateGame.current.ballR))
             {
                 // Si la balle rentre en collision avec la raquette du joueur 2 , la balle rebondit
                 stateGame.current.ballDx = -stateGame.current.ballDx;
@@ -235,7 +244,6 @@ function Game(/*props:any*/) {
     
     return (
         <>
-        {/*https://jill-jenn.net/conferences/cassebriques/move.html*/}
 
         <div className={clsx(classes.Game, ftShowGame(user))}>
             <canvas id={styles.canvas} width="800" height="400" onClick={()=>play()} tabIndex={0} onKeyDown={(event)=>keyDownHandler(event)} onKeyUp={(event)=>keyUpHandler(event)}></canvas>
