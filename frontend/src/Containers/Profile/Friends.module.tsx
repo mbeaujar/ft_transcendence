0554ab64c80b79561e986fdd986e1917c6035ed7
Ali Salam <asalam@macbook-pro-de-ali.home>
2022-02-14 08:30:04 +0100

    frineds debut

--- frontend/src/Containers/Profile/Friends.module.tsx
+++ frontend/src/Containers/Profile/Friends.module.tsx
@@ -6,14 +6,31 @@ import classes from './Friends.module.scss';
 
 function Friends()
 {
+    const [title, setTitle] = useState('');
 
     return (
         <div className={classes.Friends}>
 
-        <form>
-            <label>Search user<input type="text" name="name" /></label>
-            <input type="submit" value="Add Friends" />
-        </form>
+            <div className={classes.FriendsLeft}>
+                <h2>My Friends</h2>
+            </div>
+
+            
+            <div className={classes.FriendsRight}>
+                <div className={classes.AddFriends}>
+                    <h2>Add new friends</h2>
+                    <form>
+                        <label>Search user</label>
+                        <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}/>
+                        <button>Add friends</button>
+                    </form>
+                </div>
+
+                <div className={classes.FriendsRequest}>
+                    <h2>Friends Request</h2>
+                </div>
+            </div>
+
         
         </div>
     );
