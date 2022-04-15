f2db90a72e60c1ce5f0cc659586204bc7ca9810f
Ali Salam <asalam@macbook-pro-de-ali.home>
2022-04-15 02:32:30 +0200

    refonte component : stats ok

--- frontend/src/Containers/Profile/components/Stats/Stats.module.tsx
+++ frontend/src/Containers/Profile/components/Stats/Stats.module.tsx
@@ -18,14 +18,7 @@ interface Props {
 }
 
 const Stats: React.FC<Props> = (props: Props): JSX.Element => {
-  const [friendsList, setFriendsList] = useState<IFriends>();
-
-  useEffect(() => {
-    api
-      .get('/friends/list')
-      .then((response) => setFriendsList(response.data))
-      .catch((reject) => console.error(reject));
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <div className={classes.Stats}>
@@ -38,51 +31,7 @@ const Stats: React.FC<Props> = (props: Props): JSX.Element => {
       <div className={classes.Bottom}>
         <HistoryBlock />
         <FriendsBlock />
-        {/*<div className={classes.FriendsBlock}>
-          <h3 className={classes.title}>Friends</h3>
-          <div className={classes.Friends}>
-            {friendsList && (
-              <div className={classes.list}>
-                {friendsList.friends.map((friend: IUser) => (
-                  <div className={classes.friendsListElement} key={friend.id}>
-                    <Avatar user={friend} />
-                    <p>{friend.username}</p>
-                  </div>
-                ))}
-                <div className={classes.friendsListElement}>
-                  <div>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa_ZFA7Nc5_IkQinevA7dBIwquje91csViyQ&usqp=CAU" />
-                  </div>
-                  <p>Eren</p>
-                </div>
-                <div className={classes.friendsListElement}>
-                  <div>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSppkq9EOR9dtyDFm5mDlJ0-eJ2ddp8G9MSVw&usqp=CAU" />
-                  </div>
-                  <p>Livaï</p>
-                </div>
-                <div className={classes.friendsListElement}>
-                  <div>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRyhWqTEVwIMyW5Mx90y44YZjlkPVH-dm908g&usqp=CAU" />
-                  </div>
-                  <p>Erwin</p>
-                </div>
-                <div className={classes.friendsListElement}>
-                  <div>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwD1TQPCWvR6RTQ9SNgeRnw0tlF4QoUitDmg&usqp=CAU" />
-                  </div>
-                  <p>Rick</p>
-                </div>
-                <div className={classes.friendsListElement}>
-                  <div>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQww5NXS3n-jtIJeol6l462l_Nl-X9BUH6vLw&usqp=CAU" />
-                  </div>
-                  <p>Jon</p>
-                </div>
-              </div>
-            )}
-          </div>
-        </div>*/}
+        
       </div>
     </div>
   );
