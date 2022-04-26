ca06f9358d79e8a2202f09f72ee56cd998692964
Ali Salam <asalam@macbook-pro-de-ali.home>
2022-04-26 13:40:41 +0200

    decoupage statsgeneral ok

--- frontend/src/Containers/Profile/components/Stats/Stats.tsx
+++ frontend/src/Containers/Profile/components/Stats/Stats.tsx
@@ -1,17 +1,17 @@
-import React, { useEffect, useState, useRef } from 'react';
-import api from '../../../../apis/api';
-import clsx from 'clsx';
-import './Stats.scss';
-import styles from './Stats.module.scss';
-import { IUser } from '../../../../interface/user.interface';
-import { IFriends } from '../../../../interface/friends.interface';
-import Avatar from '../Avatar/Avatar';
-import Pongopoints from './Components/Pongopoints/Pongopoints';
-import Ratio from './Components/Ratio/Ratio';
-import Rank from './Components/Rank/Rank';
-import Level from './Components/Level/Level';
-import HistoryBlock from './Components/HistoryBlock/HistoryBlock';
-import FriendsBlock from './Components/FriendsBlock/FriendsBlock';
+import React, { useEffect, useState, useRef } from "react";
+import api from "../../../../apis/api";
+import clsx from "clsx";
+import "./Stats.scss";
+import styles from "./Stats.module.scss";
+import { IUser } from "../../../../interface/user.interface";
+import { IFriends } from "../../../../interface/friends.interface";
+import Avatar from "../Avatar/Avatar";
+import Pongopoints from "./Components/Pongopoints/Pongopoints";
+import Ratio from "./Components/Ratio/Ratio";
+import Rank from "./Components/Rank/Rank";
+import Level from "./Components/Level/Level";
+import HistoryBlock from "./Components/HistoryBlock/HistoryBlock";
+import FriendsBlock from "./Components/FriendsBlock/FriendsBlock";
 
 interface Props {
   user: IUser;
@@ -23,15 +23,18 @@ const Stats: React.FC<Props> = (props: Props): JSX.Element => {
   return (
     <div className="Stats">
       <div className="StatsGeneral">
-        <Pongopoints user={props.user}/>
-        <Ratio user={props.user}/>
-        <Rank user={props.user}/>
-        <Level user={props.user}/>
+        <div className="StatsGeneralLeft">
+          <Pongopoints user={props.user} />
+          <Ratio user={props.user} />
+        </div>
+        <div className="StatsGeneralRight">
+          <Rank user={props.user} />
+          <Level user={props.user} />
+        </div>
       </div>
       <div className="Bottom">
-        <HistoryBlock user={props.user}/>
+        <HistoryBlock user={props.user} />
         <FriendsBlock />
-        
       </div>
     </div>
   );
