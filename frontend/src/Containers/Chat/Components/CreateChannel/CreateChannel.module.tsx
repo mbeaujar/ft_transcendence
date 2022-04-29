b083aaf08593dab062be4f99e2f8586b25532bad
Ali Salam <asalam@macbook-pro-de-ali.home>
2022-04-29 04:46:33 +0200

    securite channelname ok

--- frontend/src/Containers/Chat/Components/CreateChannel/CreateChannel.module.tsx
+++ frontend/src/Containers/Chat/Components/CreateChannel/CreateChannel.module.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
-import { IChannel } from '../../../../interface/channel.interface';
-import { IUser } from '../../../../interface/user.interface';
-import { Scope } from '../../../../interface/scope.enum';
-import Checkbox from '../Checkbox/Checkbox.module';
-import classes from './CreateChannel.module.scss';
+import React, { useState } from "react";
+import { IChannel } from "../../../../interface/channel.interface";
+import { IUser } from "../../../../interface/user.interface";
+import { Scope } from "../../../../interface/scope.enum";
+import Checkbox from "../Checkbox/Checkbox.module";
+import classes from "./CreateChannel.module.scss";
+import { toast } from "react-toastify";
 
 interface Props {
   user: IUser;
@@ -11,9 +12,28 @@ interface Props {
 }
 
 const CreateChannel: React.FC<Props> = (props: Props): JSX.Element => {
-  const [name, setName] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const [scope, setScope] = useState<Scope>(Scope.public);
+
+  function isValidChannelName(newName: string) {
+    let i = 0;
+    while (i < newName.length) {
+      if (
+        !(
+          (newName.charCodeAt(i) >= 48 && newName.charCodeAt(i) <= 57) ||
+          (newName.charCodeAt(i) >= 65 && newName.charCodeAt(i) <= 90) ||
+          (newName.charCodeAt(i) >= 97 && newName.charCodeAt(i) <= 122) ||
+          newName.charCodeAt(i) == 45 ||
+          newName.charCodeAt(i) == 95
+        )
+      ) {
+        return false;
+      }
+      i++;
+    }
+    return true;
+  }
 
   return (
     <div className={classes.CreateChannel}>
@@ -29,26 +49,26 @@ const CreateChannel: React.FC<Props> = (props: Props): JSX.Element => {
       <div className={classes.ChannelType}>
         <h2>Type of channel</h2>
         <div className={classes.ChannelCheckbox}>
-        <div className={classes.CheckboxPublic}>
-          <Checkbox
-            label="Public"
-            value={scope === Scope.public}
-            onChange={() => setScope(Scope.public)}
-          />
+          <div className={classes.CheckboxPublic}>
+            <Checkbox
+              label="Public"
+              value={scope === Scope.public}
+              onChange={() => setScope(Scope.public)}
+            />
           </div>
           <div className={classes.CheckboxPrivate}>
-          <Checkbox
-            label="Private"
-            value={scope === Scope.private}
-            onChange={() => setScope(Scope.private)}
-          />
+            <Checkbox
+              label="Private"
+              value={scope === Scope.private}
+              onChange={() => setScope(Scope.private)}
+            />
           </div>
           <div className={classes.CheckboxProtected}>
-          <Checkbox
-            label="Protected"
-            value={scope === Scope.protected}
-            onChange={() => setScope(Scope.protected)}
-          />
+            <Checkbox
+              label="Protected"
+              value={scope === Scope.protected}
+              onChange={() => setScope(Scope.protected)}
+            />
           </div>
         </div>
       </div>
@@ -63,18 +83,27 @@ const CreateChannel: React.FC<Props> = (props: Props): JSX.Element => {
         </div>
       ) : null}
       <div>
-        <button className={classes.CreateChannelButton}
+        <button
+          className={classes.CreateChannelButton}
           onClick={() => {
-            const channel: IChannel = {
-              name,
-              state: scope,
-              password,
-              users: [],
-            };
-            props.socketEmit(channel);
-            setName('');
-            setPassword('');
-            setScope(Scope.public);
+            if (isValidChannelName(name) === false) {
+              toast.error(
+                "Your channel name can only containe number,letter,- and _"
+              );
+            } else if (name.length < 4 || name.length > 9) {
+              toast.error("Your channel name must contain between 4 and 9 letters");
+            } else {
+              const channel: IChannel = {
+                name,
+                state: scope,
+                password,
+                users: [],
+              };
+              props.socketEmit(channel);
+              setName("");
+              setPassword("");
+              setScope(Scope.public);
+            }
           }}
         >
           Create
