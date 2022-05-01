4a42d1d4d0d3f3a1f3e883be779ae73984e4046d
Ali Salam <asalam@macbook-pro-de-ali.home>
2022-05-01 23:58:55 +0200

    parametrage clickdroit

--- frontend/src/Containers/Chat/Components/Discussion/Discussion.module.tsx
+++ frontend/src/Containers/Chat/Components/Discussion/Discussion.module.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import classes from "./Discussion.module.scss";
 import { IUser } from "../../../../interface/user.interface";
@@ -8,6 +8,18 @@ import Avatar from "../../../Profile/components/Avatar/Avatar";
 
 import styles from "./Discussion.module.scss";
 import Input from "./Input";
+
+import {
+  Menu,
+  Item,
+  Separator,
+  Submenu,
+  useContextMenu,
+} from "react-contexify";
+
+import "react-contexify/dist/ReactContexify.css";
+
+const MENU_ID = "menu-id";
 
 interface Props {
   user: IUser;
@@ -20,6 +32,7 @@ interface Props {
 
 const Discussion: React.FC<Props> = (props: Props): JSX.Element => {
   var messageBody = document.querySelector("#msg");
+  const [userActif, setUserActif] = useState<IUser>();
 
   useEffect(() => {
     messageBody = document.querySelector("#msg");
@@ -28,13 +41,64 @@ const Discussion: React.FC<Props> = (props: Props): JSX.Element => {
     }
   }, [props.messages]);
 
-  const renderedMessages = props.messages.map((message: any) => {
+  const { show } = useContextMenu({
+    id: MENU_ID,
+  });
+
+  function handleItemClick(
+    event: any,
+    props: any,
+    triggerEvent: any,
+    data: any
+  ) {
+    console.log(event, props, triggerEvent, data);
+    console.log("okkkkk");
+  }
+
+  function RightClickProfile() {
+    console.log("RightClickProfile:", userActif);
+    let path = "/OtherUserProfile/" + userActif?.username;
+    window.location.href = path;
+  }
+
+  function RightClickBlock() {
+    console.log("RightClickBlock", userActif);
+  }
+
+  function RightClickInviteToPlay() {
+    console.log("RightClickInviteToPlay", userActif);
+  }
+
+  function displayMenu(e: any, userToVisit: IUser) {
+    console.log("usermenu=", userToVisit.username);
+    setUserActif(userToVisit);
+    // put whatever custom logic you need
+    // you can even decide to not display the Menu
+    if (userToVisit.username != props.user.username) show(e);
+  }
+
+  const renderedMessages = props.messages.map((message: any, index: number) => {
+    let userClick: IUser = message.user;
+    console.log("userclick=", props.messages);
     return (
       <div key={message.id} className={classes.Message}>
         <Avatar user={message.user} />
         <div className={classes.MessageRight}>
-          <h4>{message.user.username}</h4>
+          <h4 onContextMenu={(e) => displayMenu(e, message.user)}>
+            {message.user.username}
+          </h4>
           <p> {message.text}</p>
+          <Menu id={MENU_ID} theme="dark">
+            <Item
+              onClick={() => {
+                RightClickProfile();
+              }}
+            >
+              See profile
+            </Item>
+            <Item onClick={() => RightClickBlock()}>Block this user</Item>
+            <Item onClick={() => RightClickInviteToPlay()}>Invite to play</Item>
+          </Menu>
         </div>
       </div>
     );
@@ -42,9 +106,13 @@ const Discussion: React.FC<Props> = (props: Props): JSX.Element => {
 
   return (
     <div className={classes.Discussion}>
-      <h1>{props.channel.state!==3?props.channel.name:props.channel.users[0]?.user?.id === props.user.id
-            ? `${props.channel.users[1]?.user?.username}`
-            : `${props.channel.users[0]?.user?.username}`}</h1>
+      <h1>
+        {props.channel.state !== 3
+          ? props.channel.name
+          : props.channel.users[0]?.user?.id === props.user.id
+          ? `${props.channel.users[1]?.user?.username}`
+          : `${props.channel.users[0]?.user?.username}`}
+      </h1>
       <button
         className={classes.OpenChannelSettings}
         onClick={() => props.setShowChatRight(!props.showChatRight)}
