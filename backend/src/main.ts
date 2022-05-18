4301138807b9178059a982509dd3d1f57d7aa230
ramzi <naili.ramzi@outlook.fr>
2022-05-18 12:17:13 +0200

    test main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,9 +7,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+          app.useGlobalPipes(new ValidationPipe());
 
-  app.setGlobalPrefix('api');
+          app.setGlobalPrefix('api');
 
   app.use(cookieParser(process.env.COOKIE_SECRET));
 
