bb8db33052dbea1f7ede3fc586eb673a28985e5c
Ali Salam <asalam@macbook-pro-de-ali.home>
2022-05-26 23:33:47 +0200

    theme.tsx theme.scss ok

--- frontend/src/Containers/Profile/components/Settings/Components/Theme/Theme.tsx
+++ frontend/src/Containers/Profile/components/Settings/Components/Theme/Theme.tsx
@@ -1,23 +1,43 @@
+import clsx from 'clsx';
 import React, { useEffect, useState } from 'react';
-import './Theme.scss';
+import classes from './Theme.module.scss';
 
 export interface Props {
-  theme:string;
-  setTheme:(value: string) => void;
+  theme: string;
+  setTheme: (value: string) => void;
 }
 
 function Theme(props: Props) {
   useEffect(() => {}, []);
-  const switchTheme = () => {
-    const newTheme = props.theme === 'light' ? 'dark' : 'light';
-    props.setTheme(newTheme);
-  };
+
+  // function switchTheme (theme: string){
+  //   props.setTheme(newTheme);
+  // }
+
+  function activeTheme(theme: string) {
+    if (props.theme === theme) return classes.ActiveTheme;
+    return classes.DisactiveTheme;
+  }
   return (
-    <div className="Theme">
+    <div className={classes.Theme}>
       <h3>Theme</h3>
-            <button onClick={switchTheme}>
+      <div className={classes.ThemeBottom}>
+        <div
+          className={clsx(classes.Buttontheme, classes.DarkTheme,activeTheme('dark'))}
+          onClick={()=>props.setTheme('dark')}
+        ></div>
+        <div
+          className={clsx(classes.Buttontheme, classes.LightTheme,activeTheme('light'))}
+          onClick={()=>props.setTheme('light')}
+        ></div>
+        <div
+          className={clsx(classes.Buttontheme, classes.BlueTheme,activeTheme('blue'))}
+          onClick={()=>props.setTheme('blue')}
+        ></div>
+      </div>
+      {/* <button onClick={switchTheme}>
         Switch to {props.theme === 'light' ? 'Dark' : 'Light'} Theme
-      </button>
+      </button> */}
     </div>
   );
 }
